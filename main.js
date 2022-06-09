6097639ac2483638f90947cab09f27f30da3e5ed
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-06-09 23:19:18 +0900

    add v-for

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const app = Vue.createApp({
       image: "./assets/images/socks_blue.jpeg",
       inventory: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2232, color: "greeen" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
