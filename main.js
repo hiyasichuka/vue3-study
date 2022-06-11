93dc0046edf24afff1263f705b17bea9e60df19c
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-06-11 21:27:33 +0900

    Add @click @mouseover

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
-      image: "./assets/images/socks_blue.jpeg",
+      image: "./assets/images/socks_green.jpeg",
       inventory: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2232, color: "greeen" },
-        { id: 2235, color: "blue" },
+        {
+          id: 2234,
+          color: "greeen",
+          image: "./assets/images/socks_green.jpeg",
+        },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpeg" },
       ],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
